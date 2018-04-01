b9acce0e28d5a0e3d2c1211a3e2be327550322e3
Santanu <cushantanu@gmail.com>
2018-04-01 13:00:27 +0600

    Now Introduce ONE HIGHER-ORDER Function to write EVEN_ODD by using Predicate

--- com/shantanu/Design_pattern/EvenOdd.java
+++ com/shantanu/Design_pattern/EvenOdd.java
@@ -2,41 +2,27 @@ package com.shantanu.Design_pattern;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class EvenOdd {
 
-	private static int totalValues(List<Integer> numbers) {
+	private static int totalValues(List<Integer> numbers , 
+			Predicate<Integer> selectors) {
 		int results = 0;
 		for (int i : numbers) {
+			if(selectors.test(i))
 			results += i;
 		}
 		return results;
 	}
 	
-	private static int totalEvenValues(List<Integer> numbers) {
-		int results = 0;
-		for (int i : numbers) {
-			if(i%2 ==0)
-			 results += i;
-		}
-		return results;
-	}
-	
-	private static int totalOddValues(List<Integer> numbers) {
-		int results = 0;
-		for (int i : numbers) {
-			if(i%2 !=0)
-			 results += i;
-		}
-		return results;
-	}
-
 	public static void main(String[] args) {
 		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-		System.out.println(totalValues(numbers));
-		System.out.println(totalEvenValues(numbers));
-		System.out.println(totalOddValues(numbers));
+		System.out.println(totalValues(numbers , e-> true));
+		System.out.println(totalValues(numbers , e-> e%2 ==0));
+		System.out.println(totalValues(numbers , e-> e%2 !=0));
+		
 	}
 
 }

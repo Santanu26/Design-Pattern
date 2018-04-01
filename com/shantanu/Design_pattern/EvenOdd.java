e5a7323a34a7476c4c9e93d5960638207c323aa9
Santanu <cushantanu@gmail.com>
2018-04-01 13:17:13 +0600

    Now , Introduce Util class to write more clean code

--- com/shantanu/Design_pattern/EvenOdd.java
+++ com/shantanu/Design_pattern/EvenOdd.java
@@ -4,26 +4,22 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
+import com.shantanu.Design_pattern.util.Util;
+
 public class EvenOdd {
 
 	private static int totalValues(List<Integer> numbers, Predicate<Integer> selectors) {
-		/*int results = 0;
-		for (int i : numbers) {
-			if (selectors.test(i))
-				results += i;
-		}*/
 		return numbers.stream()
 				.filter(selectors)
 				.mapToInt(e->e)
 				.sum();
 	}
-
 	public static void main(String[] args) {
 		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
 		System.out.println(totalValues(numbers, e -> true));
-		System.out.println(totalValues(numbers, e -> e % 2 == 0));
-		System.out.println(totalValues(numbers, e -> e % 2 != 0));
+		System.out.println(totalValues(numbers, Util:: isEven));
+		System.out.println(totalValues(numbers, Util:: isOdd));
 
 	}
 
---
+++ com/shantanu/Design_pattern/util/Util.java
@@ -0,0 +1,12 @@
+package com.shantanu.Design_pattern.util;
+
+public class Util {
+	public static boolean isEven(int number) {
+		return number % 2 == 0;
+	}
+
+	public static boolean isOdd(int number) {
+		return number % 2 != 0;
+	}
+
+}

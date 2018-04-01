76fe7c8a0af1100e0831d63a0522fd5fd00dd73e
Santanu <cushantanu@gmail.com>
2018-04-01 13:05:22 +0600

    Now use stream to write EVEN-ODD: Here the strategy we use it iterator design pattern

--- com/shantanu/Design_pattern/EvenOdd.java
+++ com/shantanu/Design_pattern/EvenOdd.java
@@ -6,23 +6,24 @@ import java.util.function.Predicate;
 
 public class EvenOdd {
 
-	private static int totalValues(List<Integer> numbers , 
-			Predicate<Integer> selectors) {
-		int results = 0;
+	private static int totalValues(List<Integer> numbers, Predicate<Integer> selectors) {
+		/*int results = 0;
 		for (int i : numbers) {
-			if(selectors.test(i))
-			results += i;
-		}
-		return results;
+			if (selectors.test(i))
+				results += i;
+		}*/
+		return numbers.stream()
+				.filter(selectors)
+				.reduce(0, Integer :: sum);
 	}
-	
+
 	public static void main(String[] args) {
 		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-		System.out.println(totalValues(numbers , e-> true));
-		System.out.println(totalValues(numbers , e-> e%2 ==0));
-		System.out.println(totalValues(numbers , e-> e%2 !=0));
-		
+		System.out.println(totalValues(numbers, e -> true));
+		System.out.println(totalValues(numbers, e -> e % 2 == 0));
+		System.out.println(totalValues(numbers, e -> e % 2 != 0));
+
 	}
 
 }

ef4dc2c37874e91cf0fe80e4ee6b44f19ec9e903
Santanu <cushantanu@gmail.com>
2018-04-01 22:59:43 +0600

    Here is implemented more refactoring for lambda

--- com/shantanu/Design_pattern/decorator/Camera.java
+++ com/shantanu/Design_pattern/decorator/Camera.java
@@ -6,15 +6,26 @@ import java.util.stream.Stream;
 
 @SuppressWarnings("unchecked")
 public class Camera {
-	private Function<Color , Color> filter;
-	public Camera(Function<Color , Color> ... filters) {
+	private Function<Color, Color> filter;
+
+	public Camera(Function<Color, Color>... filters) {
 		setFilter(filters);
 	}
-	public void setFilter(Function<Color , Color> ... filters) {
+
+	public void setFilter(Function<Color, Color>... filters) {
 		filter = Stream.of(filters)
-				.reduce(color->color,
-				  (theFilters , aFilter) -> theFilters.andThen(aFilter));
+				.reduce(Function.identity(), Function :: andThen);
+
+		// introduced identity functions
+
+		// (theFilters , aFilters) -> theFilters.andThen(aFilter);
+		// If we take a filter and pass it to another
+		// then we can use method reference instead of lambda
+		
+		//instead of using this lambda use simple 
+		// Function :: andThen;
 	}
+
 	public Color snap(Color color) {
 		return filter.apply(color);
 	}
